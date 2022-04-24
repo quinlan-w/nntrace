550175f8b1f8382eb716fd931fe3e56cb8e4471a
Zhiquan Wang <wangzhiquan@axera-tech.com>
2022-04-24 23:06:15 +0800

    test

--- bin2json.cpp
+++ bin2json.cpp
@@ -1,59 +1,50 @@
-#include <iostream>
+#include "nntrace.h"
 #include <fstream>
+#include <iostream>
+#include <map>
+#include <set>
 #include <string>
 #include <vector>
-#include <set>
-#include <map>
-#include "nntrace.h"
 using namespace std;
 
-int test_channels(int argc, char **argv)
-{
-    if (argc != 3)
-    {
-        printf("useage: bin2json neusight_data.bin trace.json\n");
-        return 0;
-    }
-    ifstream inFile(argv[1], ios::binary);
-    if (!inFile)
-    {
-        cout << "error" << endl;
-        return 0;
-    }
-    int len = 0;
-    if (inFile.is_open())
-    {
-        inFile.seekg(0, ios::end);
-        len = inFile.tellg();
-        inFile.seekg(0, ios::beg);
-    }
-
-    char *neusight_data = new char[len];
-    inFile.read(neusight_data, len);
-    inFile.close();
-
-    vector<string> json = get_nntrace(neusight_data, len);
-
-    // for (auto iter = json.begin(); iter != json.end(); ++iter)
-    // {
-    //     cout << iter->c_str();
-    // }
-    ofstream outFile(argv[2]);
-    for (auto iter = json.begin(); iter != json.end(); ++iter)
-    {
-        outFile.write(iter->data(), iter->size());
-    }
-    outFile.close();
+int test_channels(int argc, char** argv) {
+  if (argc != 3) {
+    printf("useage: bin2json neusight_data.bin trace.json\n");
     return 0;
+  }
+  ifstream inFile(argv[1], ios::binary);
+  if (!inFile) {
+    cout << "error" << endl;
+    return 0;
+  }
+  int len = 0;
+  if (inFile.is_open()) {
+    inFile.seekg(0, ios::end);
+    len = inFile.tellg();
+    inFile.seekg(0, ios::beg);
+  }
+
+  char* neusight_data = new char[len];
+  inFile.read(neusight_data, len);
+  inFile.close();
+
+  vector<string> json = get_nntrace(neusight_data, len);
+
+  // for (auto iter = json.begin(); iter != json.end(); ++iter)
+  // {
+  //     cout << iter->c_str();
+  // }
+  ofstream outFile(argv[2]);
+  for (auto iter = json.begin(); iter != json.end(); ++iter) {
+    outFile.write(iter->data(), iter->size());
+  }
+  outFile.close();
+  return 0;
 }
 
-int test_bandwidth()
-{
-    return 0;
-}
+int test_bandwidth() { return 0; }
 
-int main(int argc, char **argv)
-{
-    test_channels(argc, argv);
-    return 0;
+int main(int argc, char** argv) {
+  test_channels(argc, argv);
+  return 0;
 }
--- txt2bin.cpp
+++ txt2bin.cpp
@@ -1,29 +1,27 @@
-#include <iostream>
 #include <fstream>
-#include<vector>
+#include <iostream>
+#include <vector>
 using namespace std;
 typedef unsigned long long int uint64;
 
-int main(int argc, char ** argv)
-{
-    if (argc != 3) 
-    {
-        printf("useage: txt2bin neusight.txt neusight_data.bin\n");
-        return 0;
-    }
-    ifstream inFile(argv[1]);
-    if (!inFile) {
-        cout << "error" << endl;
-        return 0;
-    }
-    char temp[16];
-    uint64 buf = 0;
-    ofstream outFile(argv[2], ios::binary|ios::trunc);
-    while (inFile >> temp) {
-        sscanf(temp, "%llx", &buf);
-        outFile.write((char *)&buf, sizeof(buf));
-    }
-    inFile.close();
-    outFile.close();
+int main(int argc, char** argv) {
+  if (argc != 3) {
+    printf("useage: txt2bin neusight.txt neusight_data.bin\n");
     return 0;
+  }
+  ifstream inFile(argv[1]);
+  if (!inFile) {
+    cout << "error" << endl;
+    return 0;
+  }
+  char temp[16];
+  uint64 buf = 0;
+  ofstream outFile(argv[2], ios::binary | ios::trunc);
+  while (inFile >> temp) {
+    sscanf(temp, "%llx", &buf);
+    outFile.write((char*)&buf, sizeof(buf));
+  }
+  inFile.close();
+  outFile.close();
+  return 0;
 }
